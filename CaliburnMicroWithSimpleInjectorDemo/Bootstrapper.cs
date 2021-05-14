ebaaba3d7367ced1b1d5db0001d50116ab2a0b92
Niels van Strien <nvanstrien.phd@gmail.com>
2021-05-14 07:06:04 +0200

    Add project files.

---
+++ CaliburnMicroWithSimpleInjectorDemo/Bootstrapper.cs
@@ -0,0 +1,73 @@
+﻿using Caliburn.Micro;
+using CaliburnMicroWithSimpleInjectorDemo.ViewModels;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnMicroWithSimpleInjectorDemo
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public static readonly Container _container = new();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Register<IWindowManager, WindowManager>();
+            _container.RegisterSingleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterSingleton(viewModelType, viewModelType));
+
+            _container.Verify();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+
+            //_container.GetAllInstances(service);
+
+            IServiceProvider provider = _container;
+
+            Type collectionType = typeof(IEnumerable<>).MakeGenericType(service);
+
+            IEnumerable<object> services = (IEnumerable<object>)provider.GetService(collectionType);
+
+            return services ?? Enumerable.Empty<object>();
+        }
+
+        protected override object GetInstance(System.Type service, string key)
+        {
+            return _container.GetInstance(service);
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return new[] { Assembly.GetExecutingAssembly() };
+        }
+
+
+        protected override void BuildUp(object instance)
+        {
+            InstanceProducer registration = _container.GetRegistration(instance.GetType(), true);
+            registration.Registration.InitializeInstance(instance);
+        }
+    }
+}
+
---
+++ CaliburnMicroWithSimpleInjectorDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnMicroWithSimpleInjectorDemo.ViewModels
+{
+    public class ShellViewModel
+    {
+        public async Task Button1()
+        {
+            Debug.Print("Hello world");
+
+            var data = await GetSampleDataAsync();
+
+            foreach (var item in data)
+            {
+                Debug.Print(item);
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetSampleDataAsync()
+        {
+            // method simulating getting async data
+            var data = new List<string>() { "hello", "world" };
+
+            return await Task.FromResult(data);
+        }
+
+    }
+}
